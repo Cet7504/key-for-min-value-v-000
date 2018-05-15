36571c6798738f251c7337bb9032d652a7a36c1e
Christian Timberlake II <timberlakece1@my.palmbeachstate.edu>
2018-05-15 00:56:04 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = 0
 
-  hash.each do |key, value|
+  name_hash.each do |key, value|
     if lowest_value == 0 || lowest_value > value
       lowest_value = value
       lowest_key = key
